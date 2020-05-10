7078c8124fa31515b48c17b8863b3c1cdc6a7529
Victor Gonzalez <dazedbutamused@gmail.com>
2020-05-09 23:53:09 -0700

    more config

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 router.get('/test', (req, res) => {
   res.send('this app works!!!');
-})
+});
 
 module.exports = router;
